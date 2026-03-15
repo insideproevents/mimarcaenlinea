a19a1be8f5140358224a6d89363d4311b68b3db5
Ignacio <insideproevents@gmail.com>
2026-03-14 22:24:52 -0300

    fix: remove bubble animation class (no blink)

--- src/components/InteractiveRobot.tsx
+++ src/components/InteractiveRobot.tsx
@@ -136,7 +136,7 @@ export function InteractiveRobot() {
 
           {/* Text bubble on click */}
           {showBubble && (
-            <div key="bubble" className="absolute -top-16 left-1/2 md:-top-20 -translate-x-1/2 mb-2 w-48 md:w-[18rem] z-20 pointer-events-none animate-bounce-in" style={{ animationDelay: '0.1s' }}>
+            <div key="bubble" className="absolute -top-16 left-1/2 md:-top-20 -translate-x-1/2 mb-2 w-48 md:w-[18rem] z-20 pointer-events-none" style={{ animationDelay: '0.1s' }}>
                 <div 
                   className="relative w-full h-24 md:h-28 bg-no-repeat bg-center bg-contain rounded-xl flex items-center justify-center overflow-hidden"
                   style={{
