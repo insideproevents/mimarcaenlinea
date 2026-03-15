f56cdcfc6f660e96b4a94e8747ddebe0a2feabaa
Ignacio <insideproevents@gmail.com>
2026-03-14 22:07:43 -0300

    feat: increase bubble size 20% (w-48/w-18rem, h-24/28, -top-24/-32)

--- src/components/InteractiveRobot.tsx
+++ src/components/InteractiveRobot.tsx
@@ -128,9 +128,9 @@ export function InteractiveRobot() {
 
           {/* Text bubble on click */}
           {showBubble && (
-            <div className="absolute -top-20 left-1/2 md:-top-28 -translate-x-1/2 mb-3 w-[10rem] md:w-72 z-20 pointer-events-none animate-bounce-in" style={{ animationDelay: '0.1s' }}>
+            <div className="absolute -top-24 left-1/2 md:-top-32 -translate-x-1/2 mb-4 w-48 md:w-[18rem] z-20 pointer-events-none animate-bounce-in" style={{ animationDelay: '0.1s' }}>
               <div 
-                className="relative w-full h-20 md:h-24 bg-no-repeat bg-center bg-contain rounded-xl flex items-center justify-center overflow-hidden"
+                className="relative w-full h-24 md:h-28 bg-no-repeat bg-center bg-contain rounded-xl flex items-center justify-center overflow-hidden"
                 style={{
                   backgroundImage: "url('/assetts/burbuja.png')",
                   backgroundSize: 'contain',
