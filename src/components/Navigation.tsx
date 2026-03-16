0e57e3075c89cb4a3595d3072006a638a819e725
Ignacio <insideproevents@gmail.com>
2026-03-16 15:05:58 -0300

    fix: reduce logo_texto size by 20% on mobile

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -60,7 +60,7 @@ export function Navigation() {
               <img 
                 src="/logo_texto.png" 
                 alt="MIMARCA" 
-                className="h-[62px] sm:h-[64px] w-auto"
+                className="h-[50px] sm:h-[64px] w-auto"
               />
             </a>
 
